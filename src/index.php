dac7140c7c7df3aba44ea96343a1462f2a0ef591
Thomas Gourouza <thomas.gourouza@de.markant.com>
2023-03-07 18:00:06 +0100

    update

--- src/index.php
+++ src/index.php
@@ -12,33 +12,35 @@ $title = "Site de Vika";
 ?>
 
 <html>
-
 <head>
     <meta charset="utf-8">
     <title><?php echo $title; ?></title>
     <link rel="icon" href="./favicon.ico">
     <link href="./style.css" rel="stylesheet" type="text/css">
     <!-- Bootstrap -->
-    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD"
+          crossorigin="anonymous"
+    >
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
+            crossorigin="anonymous"
+    ></script>
+    <!-- jQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
 </head>
-
 <body>
     <header>
         <?php include "components/header/header.php"; ?>
     </header>
     <main>
         <article>
-            <div id="site-logo"></div>
-            <div class="transparent">
-                <?php include "components/" . $page . "/" . $page . ".php"; ?>
-            </div>
+            <?php include "components/" . $page . "/" . $page . ".php"; ?>
         </article>
     </main>
     <footer>
         <?php include "components/footer/footer.php"; ?>
     </footer>
 </body>
-
 </html>
