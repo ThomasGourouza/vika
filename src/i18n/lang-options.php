b0b19c2df6a1dd1c73319b95f8b36cf04bb389ad
Thomas Gourouza <thomas.gourouza@de.markant.com>
2023-03-08 16:24:00 +0100

    update

--- src/i18n/lang-options.php
+++ src/i18n/lang-options.php
@@ -5,21 +5,29 @@ class LangOption {
     public $name;
 }
 
+//############ Languages ############
+
+// French
 $frOption = new LangOption();
 $frOption->code = 'fr';
 $frOption->name = 'Français';
 
+// English
 $enOption = new LangOption();
 $enOption->code = 'en';
 $enOption->name = 'English';
 
+// Russian
 $ruOption = new LangOption();
 $ruOption->code = 'ru';
 $ruOption->name = 'Русский';
 
+// German
 $deOption = new LangOption();
 $deOption->code = 'de';
 $deOption->name = 'Deutsch';
+
+//###################################
 
 $langOptions = array($frOption, $enOption, $ruOption, $deOption);
 
