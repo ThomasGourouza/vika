bf203a16d3e432108bd56f48a32a5dc816e558b7
Thomas Gourouza <thomas.gourouza@de.markant.com>
2023-03-13 16:54:51 +0100

    update

--- src/website/pages/welcome/welcome.php
+++ src/website/pages/welcome/welcome.php
@@ -8,30 +8,13 @@
 <div class="container">
     <?php
     if (count($periods) > 0) {
-        echo '<table class="table table-striped">
-        <thead><tr class="period-title">
-        <th>' . translate('id') . '</th>
-        <th>' . translate('begin') . '</th>
-        <th>' . translate('end') . '</th>
-        <th>' . translate('time') . '</th>
-        <th>' . translate('place') . '</th>
-        <th>' . translate('period_title') . '</th>
-        <th>' . translate('description') . '</th>
-        </tr></thead>
-        <tbody>';
         foreach ($periods as $period) {
-            echo '<tr>
-            <th class="period-info">' . $period->id . '</th>
-            <td class="period-info">' . localizeDate($period->begin) . '</td>
-            <td class="period-info">' . localizeDate($period->end) . '</td>
-            <td class="period-info">' . $period->getTime() . '</td>
-            <td class="period-info">' . $period->place . '</td>
-            <td class="period-info">' . $period->title . '</td>
-            <td class="period-description">' . $period->description . '</td>
-            </tr>';
+            echo '<div class="period-block">
+                <div class="period-title">' . localizeDate($period->begin) . ' - ' . localizeDate($period->end) . ' (' . $period->getTime() . ') | ' . $period->title . '</div>
+                <div class="period-info">' . $period->place . '</div>
+                <div class="period-description">' . $period->description . '</div>
+            </div>';
         }
-        echo '</tbody>
-        </table>';
     }
     ?>
 </div>
