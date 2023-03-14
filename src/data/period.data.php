627d78e139cb59fb7d8f222fe8b8dda5619cdb8f
Thomas Gourouza <thomas.gourouza@de.markant.com>
2023-03-14 17:06:52 +0100

    update

--- src/data/period.data.php
+++ src/data/period.data.php
@@ -1,6 +1,33 @@
 <?php
 
-addPeriod(dayMonthYear(25, 03, 2016), now(), "Universite de Strasbourg, France", "Charge de communication", "C'etait vraiment cool.");
-addPeriod(dayMonthYear(25, 03, 2016), now(), "Universite de Strasbourg, France", "Charge de communication", "C'etait vraiment cool.");
-addPeriod(dayMonthYear(25, 03, 2016), now(), "Universite de Strasbourg, France", "Charge de communication", "C'etait vraiment cool.");
-addPeriod(dayMonthYear(25, 03, 2016), now(), "Universite de Strasbourg, France", "Charge de communication", "C'etait vraiment cool.");
+addPeriod(
+    dayMonthYear(20, 02, 2014),
+    now(),
+    "Universite de Strasbourg, France",
+    "Caweb",
+    "C'etait pas mal"
+);
+
+addPeriod(
+    dayMonthYear(21, 04, 2018),
+    now(),
+    "Universite de Rennes, France",
+    "Professeur de langues",
+    "Pas terrible"
+);
+
+addPeriod(
+    dayMonthYear(19, 05, 2020),
+    now(),
+    "Universite de Bordeaux, France",
+    "Danceuse",
+    "Danceuse etoile"
+);
+
+addPeriod(
+    dayMonthYear(18, 06, 2022),
+    now(),
+    "Universite de Toulouse, France",
+    "Vendeuse",
+    "Vendeuse en magasin"
+);
